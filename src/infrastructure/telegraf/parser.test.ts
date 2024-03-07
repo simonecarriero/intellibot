dda68f1fddd42918f17f747fb9204d46d01ab5ce
Simone Carriero <simonecarriero@gmail.com>
2024-03-07 08:43:47 +0100

    Parse more scenarios

--- src/infrastructure/telegraf/parser.test.ts
+++ src/infrastructure/telegraf/parser.test.ts
@@ -1,5 +1,3 @@
-import { BookingRequest } from '../../domain/BookingRequest';
-import { User } from '../../domain/FreeSpot';
 import { justDate } from '../../domain/JustDate';
 import { justTime } from '../../domain/JustTime';
 import { parse } from './parser';
@@ -9,6 +7,54 @@ describe(`parser`, () => {
     const now = () => justDate(2024, 3, 1);
     const requests = parse('/book monday 18:45 - 20', now);
 
-    expect(requests).toEqual([{ date: justDate(2024, 3, 4), from: justTime(18, 45), to: justTime(20), chat: 123 }]);
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 4), from: justTime(18, 45), to: justTime(20) }),
+    ]);
+  });
+
+  it(`parses missing day as today`, async () => {
+    const now = () => justDate(2024, 3, 1);
+    const requests = parse('/book 18:45 - 20', now);
+
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 1), from: justTime(18, 45), to: justTime(20) }),
+    ]);
+  });
+
+  it(`parses missing time range as 18 to 20`, async () => {
+    const now = () => justDate(2024, 3, 1);
+    const requests = parse('/book today', now);
+
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 1), from: justTime(18), to: justTime(20) }),
+    ]);
+  });
+
+  it(`parses missing day and missing time range as today 18 to 20`, async () => {
+    const now = () => justDate(2024, 3, 1);
+    const requests = parse('/book', now);
+
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 1), from: justTime(18), to: justTime(20) }),
+    ]);
+  });
+
+  it(`parses a single time defaulting the end of the range to +2 hours`, async () => {
+    const now = () => justDate(2024, 3, 1);
+    const requests = parse('/book today 18', now);
+
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 1), from: justTime(18), to: justTime(20) }),
+    ]);
+  });
+
+  it(`parses multiple dates and time ranges`, async () => {
+    const now = () => justDate(2024, 3, 1);
+    const requests = parse('/book today 18 - 19 and tomorrow 19 - 20', now);
+
+    expect(requests).toEqual([
+      expect.objectContaining({ date: justDate(2024, 3, 1), from: justTime(18), to: justTime(19) }),
+      expect.objectContaining({ date: justDate(2024, 3, 2), from: justTime(19), to: justTime(20) }),
+    ]);
   });
 });
--- src/infrastructure/telegraf/parser.ts
+++ src/infrastructure/telegraf/parser.ts
@@ -5,11 +5,27 @@ import * as chrono from 'chrono-node';
 
 export const parse = (input: string, now: () => JustDate): BookingRequest[] => {
   const results = chrono.parse(input, new Date(formatDate(now())), { forwardDate: true });
-  const result = results[0];
 
-  const date = justDate(result.start.get('year')!, result.start.get('month')!, result.start.get('day')!);
-  const from = justTime(result.start.get('hour')!, result.start.get('minute')!);
-  const to = justTime(result.end?.get('hour')!, result.end?.get('minute')!);
+  if (results.length < 1) {
+    return [{ date: now(), from: justTime(18), to: justTime(20), chat: 123 }];
+  }
 
-  return [{ date, from, to, chat: 123 }];
+  return results.map((result) => {
+    const year = result.start.get('year') || now().year;
+    const month = result.start.get('month') || now().month;
+    const day = result.start.get('day') || now().day;
+
+    const fromHour = result.start.isCertain('hour') ? result.start.get('hour')! : 18;
+    const fromMinute = result.start.isCertain('minute') ? result.start.get('minute')! : 0;
+
+    const toHour = result.end?.get('hour') || fromHour + 2;
+    const toMinute = result.end?.get('minute') || 0;
+
+    return {
+      date: justDate(year, month, day),
+      from: justTime(fromHour, fromMinute),
+      to: justTime(toHour, toMinute),
+      chat: 123,
+    };
+  });
 };
