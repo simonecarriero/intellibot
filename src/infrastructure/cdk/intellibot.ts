ccbd5b44af8c3d0774e1fc41456f1d9ecd654ca4
Simone Carriero <simonecarriero@gmail.com>
2024-08-20 09:19:14 +0200

    Remove AWS stuff

--- src/infrastructure/cdk/intellibot.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { IntellibotStack } from './intellibot-stack';
-import * as cdk from 'aws-cdk-lib';
-import 'source-map-support/register';
-
-const app = new cdk.App();
-new IntellibotStack(app, 'IntellibotStack', {});
